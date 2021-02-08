bb3e9795ef7f6c932a2dc625cabeda687988a7c4
GEOMTeam2 <78709956+GEOMTeam2@users.noreply.github.com>
2021-02-08 14:38:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,24 @@ function animate(id){
   switch(id){
     case "main_about":
       document.getElementById("main_home").style.display = "none";
+      
       document.getElementById("main_about").style.display = "";
       var id_build = '#' + id;
+      $(id_build).animate2('slideInLeft');
     break;
     case "main_home":
       document.getElementById("main_about").style.display = "none";
+      
       document.getElementById("main_home").style.display = "";
       var id_build = '#' + id;
+      $(id_build).animate2('slideInRight');
     break;
     case "main_contact":
       document.getElementById("main_about").style.display = "none";
       document.getElementById("main_home").style.display = "none";
-      
+      document.getElementById("main_contact").style.display = "";
       var id_build = '#' + id;
+      $(id_build).animate2('slideInRight');
     break;
     default:
       console.log("none");
@@ -23,6 +28,10 @@ function animate(id){
  
 }
 
+window.onload = function(){
+
 document.getElementById('menu_home').addEventListener('click', function(){animate('main_home');}, false);
 document.getElementById('menu_about').addEventListener('click', function(){animate("main_about");}, false);
+document.getElementById('menu_contact').addEventListener('click', function(){animate("main_contact");}, false);
 
+}
